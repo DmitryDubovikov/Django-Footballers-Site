5d98d166d8a61da6f3133e10e25085ae3871ac0e
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2022-12-05 14:16:47 +0300

    admin site upgraded

--- footballers/wiki/admin.py
+++ footballers/wiki/admin.py
@@ -1,23 +1,34 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 # Register your models here.
 from .models import *
 
 
 class FootballerAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'created_at', 'photo', 'published')
+    list_display = ('id', 'name', 'created_at', 'get_html_photo', 'published')
     list_display_links = ('id', 'name')
     search_fields = ('name', 'content')
     list_editable = ('published',)
     list_filter = ('published', 'created_at')
     prepopulated_fields = {'slug': ('name',)}
+    fields = ('name', 'slug', 'country', 'content', 'photo', 'get_html_photo', 'published', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at', 'get_html_photo')
+    save_on_top = True
+
+
+    def get_html_photo(self, object):
+        if object.photo:
+            return mark_safe(f"<img src='{object.photo.url}' width=50>")
+
+    get_html_photo.short_description = "Miniature"
 
 
 class CountryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     list_display_links = ('id', 'name')
     search_fields = ('name',)
-    prepopulated_fields = {'slug': ('name', )}
+    prepopulated_fields = {'slug': ('name',)}
 
 
 admin.site.register(Footballer, FootballerAdmin)
